4a2f4bc931bac80dcca6f377f314f746d2f23a53
danny7773boudriau <73191165+danny7773boudriau@users.noreply.github.com>
2020-12-15 14:44:02 -0500

    Fichiers initiaux pour un projet MakeCode

---
+++ main.ts
@@ -0,0 +1,227 @@
+basic.forever(function () {
+    for (let index = 0; index < 3; index++) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            # # # # #
+            . . . . .
+            . . . . .
+            `)
+        basic.clearScreen()
+        basic.pause(1000)
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            # # # # #
+            . . . . .
+            . . . . .
+            `)
+        basic.clearScreen()
+        basic.pause(500)
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . .
+            `)
+        basic.clearScreen()
+        basic.pause(500)
+        for (let index = 0; index < 3; index++) {
+            basic.showLeds(`
+                . . . . .
+                . . . . .
+                . . # . .
+                . . . . .
+                . . . . .
+                `)
+            basic.clearScreen()
+            basic.pause(500)
+            basic.showLeds(`
+                . . . . .
+                . . . . .
+                . . # . .
+                . . . . .
+                . . . . .
+                `)
+            basic.clearScreen()
+            basic.pause(500)
+            basic.showLeds(`
+                . . . . .
+                . . . . .
+                # # # # #
+                . . . . .
+                . . . . .
+                `)
+            basic.clearScreen()
+            basic.pause(500)
+            for (let index = 0; index < 2; index++) {
+                basic.showLeds(`
+                    . . . . .
+                    . . . . .
+                    . . # . .
+                    . . . . .
+                    . . . . .
+                    `)
+                basic.clearScreen()
+                basic.pause(500)
+                basic.showLeds(`
+                    . . . . .
+                    . . . . .
+                    . . # . .
+                    . . . . .
+                    . . . . .
+                    `)
+                basic.clearScreen()
+                basic.pause(500)
+                for (let index = 0; index < 3; index++) {
+                    basic.showLeds(`
+                        . . . . .
+                        . . . . .
+                        . . # . .
+                        . . . . .
+                        . . . . .
+                        `)
+                    basic.clearScreen()
+                    basic.pause(500)
+                    basic.showLeds(`
+                        . . . . .
+                        . . . . .
+                        # # # # #
+                        . . . . .
+                        . . . . .
+                        `)
+                    basic.clearScreen()
+                    basic.pause(500)
+                    basic.showLeds(`
+                        . . . . .
+                        . . . . .
+                        . . # . .
+                        . . . . .
+                        . . . . .
+                        `)
+                    basic.clearScreen()
+                    basic.pause(500)
+                    for (let index = 0; index < 4; index++) {
+                        basic.showLeds(`
+                            . . . . .
+                            . . . . .
+                            . . # . .
+                            . . . . .
+                            . . . . .
+                            `)
+                        basic.clearScreen()
+                        basic.pause(500)
+                        basic.showLeds(`
+                            . . . . .
+                            . . . . .
+                            # # # # #
+                            . . . . .
+                            . . . . .
+                            `)
+                        basic.clearScreen()
+                        basic.pause(500)
+                        basic.showLeds(`
+                            . . . . .
+                            . . . . .
+                            . . # . .
+                            . . . . .
+                            . . . . .
+                            `)
+                        basic.clearScreen()
+                        basic.pause(500)
+                        basic.showLeds(`
+                            . . . . .
+                            . . . . .
+                            . . # . .
+                            . . . . .
+                            . . . . .
+                            `)
+                        basic.clearScreen()
+                        basic.pause(500)
+                        for (let index = 0; index < 2; index++) {
+                            basic.showLeds(`
+                                . . . . .
+                                . . . . .
+                                . . # . .
+                                . . . . .
+                                . . . . .
+                                `)
+                            basic.clearScreen()
+                            basic.pause(500)
+                            basic.showLeds(`
+                                . . . . .
+                                . . . . .
+                                # # # # #
+                                . . . . .
+                                . . . . .
+                                `)
+                            basic.clearScreen()
+                            basic.pause(1000)
+                            for (let index = 0; index < 2; index++) {
+                                basic.showLeds(`
+                                    . . . . .
+                                    . . . . .
+                                    # # # # #
+                                    . . . . .
+                                    . . . . .
+                                    `)
+                                basic.clearScreen()
+                                basic.pause(500)
+                                basic.showLeds(`
+                                    . . . . .
+                                    . . . . .
+                                    . . # . .
+                                    . . . . .
+                                    . . . . .
+                                    `)
+                                basic.clearScreen()
+                                basic.pause(500)
+                                for (let index = 0; index < 3; index++) {
+                                    basic.showLeds(`
+                                        . . . . .
+                                        . . . . .
+                                        # # # # #
+                                        . . . . .
+                                        . . . . .
+                                        `)
+                                    basic.clearScreen()
+                                    basic.pause(500)
+                                    basic.showLeds(`
+                                        . . . . .
+                                        . . . . .
+                                        . . # . .
+                                        . . . . .
+                                        . . . . .
+                                        `)
+                                    basic.clearScreen()
+                                    basic.pause(500)
+                                    basic.showLeds(`
+                                        . . . . .
+                                        . . . . .
+                                        . . # . .
+                                        . . . . .
+                                        . . . . .
+                                        `)
+                                    basic.clearScreen()
+                                    basic.pause(500)
+                                    for (let index = 0; index < 1; index++) {
+                                        basic.showLeds(`
+                                            . . . . .
+                                            . . . . .
+                                            . . # . .
+                                            . . . . .
+                                            . . . . .
+                                            `)
+                                        basic.clearScreen()
+                                        basic.pause(500)
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+})
